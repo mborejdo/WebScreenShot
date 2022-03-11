a3d873212451b3aaad7e2458991adc1295ab959b
Michael Borejdo <mborejdo+github@gmail.com>
2022-03-11 17:53:41 +0100

    moved

---
+++ src/bin/wss.rs
@@ -0,0 +1,100 @@
+use clap::{App, Arg, crate_version};
+use webscreenshotlib::{OutputFormat, screenshot_tab, write_screenshot};
+
+/// Handle CLI arguments.
+fn get_args() -> clap::ArgMatches<'static> {
+    // TODO: Add PDF OptionsObject.
+    return App::new("WebScreenShot")
+        .about("Take screenshots of Chrome-rendered webpages.")
+        .version(crate_version!())
+        .arg(Arg::with_name("url")
+            .help("URL or file:/// path to take a screencap of. i.e. https://example.com or file:///path/to/file.html")
+            .index(1)
+            .required(true)
+            .default_value("https://wikipedia.org"))
+        .arg(Arg::with_name("output-path")
+            .help("Local file path to save screenshot image to.")
+            .index(2)
+            .required(true)
+            .takes_value(true)
+            .default_value("/tmp/screenshot.png"))
+        .arg(Arg::with_name("browser-width")
+            .help("Width of the browser to render the webpage in.")
+            .short("w")
+            .long("width")
+            .takes_value(true)
+            .default_value("1024"))
+        .arg(Arg::with_name("browser-height")
+            .help("Height of the browser to render the webpage in.")
+            .short("h")
+            .long("height")
+            .takes_value(true)
+            .default_value("800"))
+        .arg(Arg::with_name("element")
+            .help("CSS selector of element to screenshot.")
+            .short("e")
+            .long("element")
+            .takes_value(true))
+        .arg(Arg::with_name("visible-only")
+            .help("Screenshot only what is visible from the dimensions of the browser window, rather then the entire surface of the page.")
+            .short("z")
+            .long("visible-only")
+            .takes_value(false))
+        .arg(Arg::with_name("quiet")
+            .help("Display no messages to stdout.")
+            .short("q")
+            .long("quiet")
+            .takes_value(false))
+        .arg(Arg::with_name("format")
+            .help("Format to save screenshot as. Must be one of png, jpg, or pdf.")
+            .short("f")
+            .long("format")
+            .takes_value(true)
+            .default_value("png")
+            .possible_values(&["png", "jpg", "pdf"]))
+        .arg(Arg::with_name("jpg-quality")
+            .help("Quality of jpg screenshot to output, 0-100. Will be ignored if --image-format is not set to jpg.")
+            .short("j")
+            .long("jpg-quality")
+            .takes_value(true)
+            .default_value("80"))
+        .get_matches();
+}
+
+/// Convert string CLI input to webscreenshotlib::OutputFormat enum.
+fn fmt_str_to_enum(fmt: &str) -> OutputFormat {
+    match fmt {
+        "jpg" | "JPG" => OutputFormat::JPG,
+        "png" | "PNG" => OutputFormat::PNG,
+        "pdf" | "PDF" => OutputFormat::PDF,
+        _ => panic!("--output-format must be one of png, jpg, or pdf.")
+    }
+}
+
+// TODO: Add CLI tests.
+/// Fullscreen screenshot of entire surface of given URL rendered in Chrome.
+fn main() -> Result<(), failure::Error> {
+    let args = get_args();
+    let path: &str = args.value_of("output-path").unwrap_or("/tmp/screenshot.png");
+    let quality: u8 = args.value_of("jpg-quality").unwrap_or("80").parse().unwrap();
+    let visible_only: bool = args.is_present("visibleonly");
+    let width: u16 = args.value_of("width").unwrap_or("1024").parse().unwrap();
+    let height: u16 = args.value_of("height").unwrap_or("800").parse().unwrap();
+    let element: &str = args.value_of("element").unwrap_or("");
+    write_screenshot(
+        path,
+        screenshot_tab(
+            args.value_of("url").unwrap_or("https://wikipedia.org"),
+            fmt_str_to_enum(args.value_of("format").unwrap()),
+            quality,
+            visible_only,
+            width,
+            height,
+            element,
+        )?,
+    )?;
+    if !args.is_present("quiet") {
+        println!("Screenshot saved to {}", path);
+    }
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,87 @@
+use headless_chrome::{Browser, LaunchOptionsBuilder, protocol::{target::methods::CreateTarget ,page::ScreenshotFormat}};
+use std::{thread, time};
+
+/// Formats available to encode screenshots to.
+pub enum OutputFormat {PNG, JPG, PDF}
+
+/// Take a screenshot of a webpage rendered in a Chrome tab.
+/// 
+/// ### Arguments
+/// - url: Web or file URL to render for screenshot.
+/// - format: The output format to encode the screenshot as.
+/// - quality: JPG quality. 0-100. This is ignored for PNGs and PDFs.
+/// - surface: If true, screencap the entire rended HTML. If false, screencap what is visible inside the browser viewport dimensions.
+/// - width: Width of the Chrome browser.
+/// - height: Height of the Chrome browser.
+/// - element: CSS selector to screenshot instead of the full page. Empty string screenshots whole page.
+///
+/// ### Example
+/// ```
+/// # use webscreenshotlib::{OutputFormat, screenshot_tab};
+/// # std::fs::write("/tmp/test.html", "<html><head><title>Title</title></head><body>Hello World</body></html>");
+/// # fn test_screenshot_tab() -> Result<(), failure::Error> {
+/// // Screenshot a page.
+/// let image_data = screenshot_tab(
+///   "file:////tmp/test.html",
+///   OutputFormat::PNG, 80, false, 1024, 800, "")?;
+/// # more_asserts::assert_ge!(image_data.len(), 1024*800);
+/// # return Ok(())
+/// # }
+/// ```
+// TODO: Add element screenshot feature.
+pub fn screenshot_tab(
+    url: &str,
+    format: OutputFormat,
+    quality: u8,
+    visible_only: bool,
+    width: u16,
+    height: u16,
+    element: &str)
+    -> Result<Vec<u8>, failure::Error>
+{
+    let options = LaunchOptionsBuilder::default()
+                    .sandbox(false)
+                    .build()
+                    .unwrap();
+    let browser = Browser::new(options)?;
+    let tab = browser.new_tab_with_options(CreateTarget {
+        url: url,
+        width: Some(width.into()),
+        height: Some(height.into()),
+        browser_context_id: None,
+        enable_begin_frame_control: None,
+    })?;
+    tab.navigate_to(url)?;
+    tab.wait_until_navigated()?;
+
+    // If element arg is supplied, wait for it to load. This will signal the
+    // tab.capture_screenshot method to screenshot only the element.
+    if element.is_empty() != true {
+        tab.wait_for_element(element)?;
+    }
+
+    let kmillis = time::Duration::from_millis(1000);
+    thread::sleep(kmillis);
+
+    // Take screenshot in given format, return image bytes.
+    match format {
+        OutputFormat::JPG => {
+            return Ok(tab.capture_screenshot(
+                ScreenshotFormat::JPEG(Some(quality.into())), None, !visible_only)?);
+            },
+        OutputFormat::PNG => return Ok(tab.capture_screenshot(
+            ScreenshotFormat::PNG, None, !visible_only)?),
+        OutputFormat::PDF => return Ok(
+            tab.print_to_pdf(None)?),
+    };
+}
+
+/// Write image data to a file. A simple wrapper around std::fs::write().
+pub fn write_screenshot(
+    output_path: &str,
+    img_data: Vec<u8>)
+    -> Result<(), failure::Error>
+{
+    std::fs::write(output_path, img_data)?;
+    return Ok(())
+}
